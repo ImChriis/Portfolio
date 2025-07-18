d67e68cfd252aa0d0ffaf9e657e6e0e4e2c012ca
Chris <christophermedinabarroso@gmail.com>
2025-07-18 16:20:44 -0400

    cambios

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,33 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { TagModule } from 'primeng/tag';
+import { TimelineModule } from 'primeng/timeline';
+
+interface work {
+  name: string;
+  date: string;
+}
 
 @Component({
   selector: 'app-home',
   imports: [
     ButtonModule,
     CommonModule,
-    TagModule
+    TagModule,
+    TimelineModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  works!: work[];
 
+  ngOnInit(): void {
+    this.works = [
+      { name: "Universidad Privada Dr Rafael Belloso Chacin", date: "2021 - Presente" },
+      { name: "Saint", date: "2024" },
+      { name: "La Gaita nos vuelve a unir", date: "Presente" }
+    ]
+  }
 }
