2ada9ff9f40cc507ae1ab4251fa74b92325db847
Chris <christophermedinabarroso@gmail.com>
2025-07-22 16:10:34 -0400

    modo claro, skills listo

--- src/app/@core/layout/layout.component.ts
+++ src/app/@core/layout/layout.component.ts
@@ -1,14 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    CommonModule
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
+  isDarkMode = false;
 
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { TagModule } from 'primeng/tag';
 import { TimelineModule } from 'primeng/timeline';
+import { TabsModule } from 'primeng/tabs';
+import { ProgressBarModule } from 'primeng/progressbar';
 
 interface work {
   name: string;
@@ -15,19 +17,64 @@ interface work {
     ButtonModule,
     CommonModule,
     TagModule,
-    TimelineModule
+    TimelineModule,
+    TabsModule,
+    ProgressBarModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit{
   works!: work[];
+  animatedYears: number = 0;
+  private targetYears: number = 2;
+  private animationDuration: number = 1000;
+  hasAnimated: boolean = false;
+
+  @ViewChild('experienceDiv') experienceDiv!: ElementRef;
 
   ngOnInit(): void {
+    this.animateYears();
+
     this.works = [
+      { name: "", date: "" },
       { name: "Universidad Privada Dr Rafael Belloso Chacin", date: "2021 - Presente" },
       { name: "Saint", date: "2024" },
-      { name: "La Gaita nos vuelve a unir", date: "Presente" }
+      { name: "Cátedra Gaitera", date: "Presente" },
+      { name: "Universidad José Gregorio Hernández", date: "Presente"},
+      { name: "", date: "" }
     ]
+  }
+
+    ngAfterViewInit() {
+    const observer = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && !this.hasAnimated) {
+        this.hasAnimated = true;
+        this.animateYears();
+      }
+    }, { threshold: 0.5 });
+
+    if (this.experienceDiv) {
+      observer.observe(this.experienceDiv.nativeElement);
+    }
+  }
+
+  animateYears(){
+    const start = 0;
+    const end = this.targetYears;
+    const duration = this.animationDuration;
+    const startTime = performance.now();
+
+     const animate = (currentTime: number) => {
+      const elapsed = currentTime - startTime;
+      if (elapsed < duration) {
+        this.animatedYears = +(start + (end - start) * (elapsed / duration)).toFixed(1);
+        requestAnimationFrame(animate);
+      } else {
+        this.animatedYears = end;
+      }
+    };
+
+    requestAnimationFrame(animate);
   }
 }
